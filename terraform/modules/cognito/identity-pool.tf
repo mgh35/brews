30c80d8117260e448a4bb7b1571ce76331fa2c90
mgh35 <markgeorgehiggins@hotmail.com>
2020-09-27 22:28:13 -0400

    Add dynamodb:DeleteItem perms (#35)

--- terraform/modules/cognito/identity-pool.tf
+++ terraform/modules/cognito/identity-pool.tf
@@ -1,12 +1,12 @@
 resource aws_cognito_identity_pool "users" {
-    identity_pool_name = "${var.app} ${var.env} users IDP"
-    allow_unauthenticated_identities = false
+  identity_pool_name               = "${var.app} ${var.env} users IDP"
+  allow_unauthenticated_identities = false
 
-    cognito_identity_providers {
-          client_id = aws_cognito_user_pool_client.users.id
-          provider_name = aws_cognito_user_pool.users.endpoint
-          server_side_token_check = false
-    }
+  cognito_identity_providers {
+    client_id               = aws_cognito_user_pool_client.users.id
+    provider_name           = aws_cognito_user_pool.users.endpoint
+    server_side_token_check = false
+  }
 }
 
 resource aws_iam_role "authenticated" {
@@ -51,7 +51,8 @@ resource aws_iam_role_policy "dynamodb" {
         "Action": [
           "dynamodb:GetItem",
           "dynamodb:PutItem",
-          "dynamodb:Query"
+          "dynamodb:Query",
+          "dynamodb:DeleteItem"
         ],
         "Resource": [
           "${var.dynamodb_table_arn}"
